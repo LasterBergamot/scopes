42051bc2be2cd9ce4d22a61e7a2c243feebb3db7
Andras Torok <>
2020-06-23 16:05:23 +0200

    Initial commit.

---
+++ src/main/java/com/pet/project/scopes/ScopesApplication.java
@@ -0,0 +1,13 @@
+package com.pet.project.scopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopesApplication.class, args);
+	}
+
+}
